202790b4ebd32b8e5336998ebd234c99d8dcf601
Vjeran <vjeran.zivkovic@hotmail.com>
2025-07-18 11:57:12 +0200

    Add some more calendar styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import "react-day-picker/style.css";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div
+      className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-b from-[#FFFFFF] to-[#DADADD]"
+    >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <BidCalendar />
       </main>
--- app/ui/calendar/calendar.tsx
+++ app/ui/calendar/calendar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { DayPicker, getDefaultClassNames } from "react-day-picker";
 import { IBM_Plex_Sans } from "next/font/google";
@@ -8,16 +8,37 @@ const ibmPlexSans = IBM_Plex_Sans({
   weight: ["300", "400", "500"],
 });
 
-export function BidCalendar() {
+export function BidCalendar({
+  className,
+  classNames,
+  formatters,
+  ...props
+}: React.ComponentProps<typeof DayPicker>) {
   const defaultClassNames = getDefaultClassNames();
+
   return (
     <DayPicker
       mode="range"
       navLayout="around"
-      className={`${ibmPlexSans.className} text-rdp-body`}
+      weekStartsOn={1}
+      className={`${ibmPlexSans.className} text-gray-100 ${className}`}
       classNames={{
-        caption_label: `${defaultClassNames.caption_label} text-base`
+        today: `border-amber-500`, // Add a border to today's date
+        selected: `bg-amber-500 border-amber-500 text-white`, // Highlight the selected day
+        root: `${defaultClassNames.root} rounded-xl p-5 bg-white-100`,
+        chevron: `fill-gray-60 w-4 h-4`,
+        caption_label: `${defaultClassNames.caption_label} text-primary-100 text-base uppercase font-medium`,
+        weekdays: `${defaultClassNames.weekdays} uppercase`,
+        weekday: `py-2 font-medium last:text-primary-100 text-[12px]`,
+        day: `${defaultClassNames.day} text-sm`,
+        ...classNames,
       }}
+      formatters={{
+        formatWeekdayName: (date) =>
+          date.toLocaleString("default", { weekday: "short" }),
+        ...formatters,
+      }}
+      {...props}
     />
   );
 }
