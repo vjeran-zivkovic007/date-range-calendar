9ed150652652f52484b9f9b0a10a779dafbdb02d
Vjeran <vjeran.zivkovic@hotmail.com>
2025-07-18 16:01:15 +0200

    Show Calendar inside the Popover WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { BidCalendar } from "./ui/calendar";
-import "react-day-picker/style.css";
 
 export default function Home() {
   return (
@@ -7,6 +6,7 @@ export default function Home() {
       className="font-sans items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-b from-[#FFFFFF] to-[#DADADD]"
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
         <BidCalendar />
       </main>
     </div>
--- app/ui/calendar/calendar.tsx
+++ app/ui/calendar/calendar.tsx
@@ -2,7 +2,10 @@
 
 import { DayPicker, getDefaultClassNames } from "react-day-picker";
 import { IBM_Plex_Sans } from "next/font/google";
+import * as Popover from "@radix-ui/react-popover";
 import { Footer } from "./components/footer";
+import "react-day-picker/style.css";
+import { Button } from "./components/button";
 
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -18,31 +21,40 @@ export function BidCalendar({
   const defaultClassNames = getDefaultClassNames();
 
   return (
-    <DayPicker
-      mode="range"
-      numberOfMonths={2}
-      navLayout="around"
-      weekStartsOn={1}
-      className={`${ibmPlexSans.className} text-gray-100 ${className}`}
-      classNames={{
-        today: "",
-        selected: `rdp-selected-1`,
-        root: `${defaultClassNames.root} rounded-xl p-5 bg-white-100`,
-        chevron: `fill-gray-60 w-4 h-4`,
-        caption_label: `${defaultClassNames.caption_label} text-primary-100 text-base uppercase font-medium`,
-        weekdays: `uppercase`,
-        weekday: `py-2 font-medium last:text-primary-100 text-[12px]`,
-        day: `${defaultClassNames.day} text-sm hover:bg-primary-10`,
-        footer: `${defaultClassNames.footer} pt-4`,
-        ...classNames,
-      }}
-      footer={<Footer />}
-      formatters={{
-        formatWeekdayName: (date) =>
-          date.toLocaleString("default", { weekday: "short" }),
-        ...formatters,
-      }}
-      {...props}
-    />
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <Button>Confirm</Button>
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content className="PopoverContent" sideOffset={32}>
+          <DayPicker
+            mode="range"
+            numberOfMonths={2}
+            navLayout="around"
+            weekStartsOn={1}
+            className={`${ibmPlexSans.className} text-gray-100 ${className}`}
+            classNames={{
+              today: "",
+              selected: `rdp-selected-1`,
+              root: `${defaultClassNames.root} rounded-xl p-5 bg-white-100`,
+              chevron: `fill-gray-60 w-4 h-4`,
+              caption_label: `${defaultClassNames.caption_label} text-primary-100 text-base uppercase font-medium`,
+              weekdays: `uppercase`,
+              weekday: `py-2 font-medium last:text-primary-100 text-[12px]`,
+              day: `${defaultClassNames.day} text-sm hover:bg-primary-10`,
+              footer: `${defaultClassNames.footer} pt-4`,
+              ...classNames,
+            }}
+            footer={<Footer />}
+            formatters={{
+              formatWeekdayName: (date) =>
+                date.toLocaleString("default", { weekday: "short" }),
+              ...formatters,
+            }}
+            {...props}
+          />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
